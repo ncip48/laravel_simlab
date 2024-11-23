4b7c8f2fe39d793688a8dd5ae0583c350fabc8eb
ncip48 <mbahcip00@gmail.com>
2024-11-23 11:24:22 +0700

    heade

--- resources/js/layouts/authenticated-layout.tsx
+++ resources/js/layouts/authenticated-layout.tsx
@@ -1,17 +1,22 @@
 import { PropsWithChildren, ReactNode } from "react";
-import {AppSidebar} from "@/components/app-sidebar";
-import {SidebarInset, SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar";
-import {Separator} from "@/components/ui/separator";
+import { AppSidebar } from "@/components/app-sidebar";
+import {
+    SidebarInset,
+    SidebarProvider,
+    SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { Separator } from "@/components/ui/separator";
 import {
     Breadcrumb,
     BreadcrumbItem,
-    BreadcrumbList, BreadcrumbPage,
+    BreadcrumbList,
+    BreadcrumbPage,
 } from "@/components/ui/breadcrumb";
 import AppearanceDropdown from "@/components/appearance-dropdown";
 
 export default function AuthenticatedLayout({
     header,
-    children
+    children,
 }: PropsWithChildren<{
     header?: ReactNode;
 }>) {
@@ -20,10 +25,13 @@ export default function AuthenticatedLayout({
             <AppSidebar />
 
             <SidebarInset>
-                <header className="sticky top-0 bg-background flex h-16 shrink-0 items-center gap-2 justify-between p-4 border-b md:border-none md:rounded-xl">
+                <header className="sticky top-0 bg-background flex h-16 shrink-0 items-center gap-2 justify-between p-4 border-b md:border-none md:rounded-xl z-50">
                     <div className="flex items-center gap-2">
                         <SidebarTrigger className="-ml-1" />
-                        <Separator orientation="vertical" className="mr-2 h-4" />
+                        <Separator
+                            orientation="vertical"
+                            className="mr-2 h-4"
+                        />
                         <Breadcrumb>
                             <BreadcrumbList>
                                 <BreadcrumbItem>
@@ -37,9 +45,7 @@ export default function AuthenticatedLayout({
                     </div>
                 </header>
 
-                <main className="p-4 md:pt-0 h-full">
-                    {children}
-                </main>
+                <main className="p-4 md:pt-0 h-full">{children}</main>
             </SidebarInset>
         </SidebarProvider>
     );
