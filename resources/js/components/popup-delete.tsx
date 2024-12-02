569feadc6092d47b2cd767eb84a43c4474fcdb03
herlyplanviu <herly@planviu.com>
2024-12-02 21:19:49 +0700

    btn

--- resources/js/components/popup-delete.tsx
+++ resources/js/components/popup-delete.tsx
@@ -8,28 +8,35 @@ import {
     AlertDialogHeader,
     AlertDialogTitle,
     AlertDialogTrigger,
-  } from "@/components/ui/alert-dialog"
-  import { Button } from "@/components/ui/button"
-import React from "react"
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import React, { useState } from "react";
 
-  export function PopupDelete({children, onSubmit}:{children:React.ReactNode, onSubmit:any}) {
+export function PopupDelete({
+    children,
+    onSubmit,
+}: {
+    children: React.ReactNode;
+    onSubmit: any;
+}) {
+    // const [open, setOpen] = useState(true);
     return (
-      <AlertDialog>
-        <AlertDialogTrigger asChild>
-          {children}
-        </AlertDialogTrigger>
-        <AlertDialogContent>
-          <AlertDialogHeader>
-            <AlertDialogTitle>Yakin hapus data?</AlertDialogTitle>
-            <AlertDialogDescription>
-              Data terhapus permanen! Tidak dapat dikembalikan!
-            </AlertDialogDescription>
-          </AlertDialogHeader>
-          <AlertDialogFooter>
-            <AlertDialogCancel>Batal</AlertDialogCancel>
-            <AlertDialogAction onClick={onSubmit}>Ya</AlertDialogAction>
-          </AlertDialogFooter>
-        </AlertDialogContent>
-      </AlertDialog>
-    )
-  }
+        <AlertDialog>
+            <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTitle>Yakin hapus data?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                        Data terhapus permanen! Tidak dapat dikembalikan!
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>Batal</AlertDialogCancel>
+                    <AlertDialogAction onClick={onSubmit} type="button">
+                        Ya
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    );
+}
